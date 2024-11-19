1235b5876eb5558f952c3e71b108527a57c88de8
Barbara Tracanella <barbara.tracanella@gmail.com>
2024-11-18 23:15:31 -0300

    Delete src directory

--- src/main/java/com/exemplo/plataformacursos/repository/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.plataformacursos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.exemplo.plataformacursos.model.Curso;
-
-@Repository
-public interface CursoRepository extends JpaRepository<Curso, Long> {}
